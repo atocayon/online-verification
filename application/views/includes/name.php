f050f31d557d3d06fc01f825e315643c1fa40ebc
Aljon <actwhitehat27@gmail.com>
2019-10-01 11:19:22 +0800

    name page

--- application/views/includes/name.php
+++ application/views/includes/name.php
@@ -6,16 +6,22 @@
 <div class="row verifyByName-form-container">
   <div class="form">
     <form>
-      <label>First Name</label>
-      <input type="text" name="fname" class="form-input" required >
+      <label>First Name</label><br>
+      <input type="text" name="fname" class="form-input-name" placeholder="Type here..." required >
+      <br>
+      <br>
+      <label>Last Name</label><br>
+      <input type="text" name="lname" class="form-input-name" placeholder="Type here..." required >
+      <br>
+      <br>
+      <label>Birthday</label><br>
+      <input type="date" name="bday" class="form-input-date" required >
+      <br>
+      <br>
+      <center>
+        <button type="submit" class="submit-button">Verify</button>
+      </center>
 
-      <label>Last Name</label>
-      <input type="text" name="lname" class="form-input" required >
-
-      <label>Birthday</label>
-      <input type="date" name="bday" class="form-input" required >
-
-      <button type="submit" class="submit-button">Verify</button>
     </form>
   </div>
   <div class="result"></div>

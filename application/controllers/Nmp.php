2763401fdadbdb4b2fddd5e91c1e518181354bbe
Aljon <actwhitehat27@gmail.com>
2019-10-02 15:40:49 +0800

    controller & model

--- application/controllers/Nmp.php
+++ application/controllers/Nmp.php
@@ -2,6 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Nmp extends CI_Controller {
+	public function __construct() {
+	  parent::__construct();
+	  $this->load->model('queries');
+	}
 
 	public function index()
 	{
@@ -11,5 +15,9 @@ class Nmp extends CI_Controller {
 	public function page(){
 		$page['id'] =  $this->uri->segment(3);
 		$this->load->view('index', $page);
+	}
+
+	public function insert_data(){
+
 	}
 }

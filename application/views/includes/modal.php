0ef5e6cdb649849d537d2bd9c39bccb97a81f24b
Aljon Tocayon <actwhiteHat27@gmail.com>
2019-11-06 14:34:21 +0800

    major fixing the ui responsiveness

--- application/views/includes/modal.php
+++ application/views/includes/modal.php
@@ -1,33 +1,39 @@
 <div class="close" ><a href=""  id="close-modal">X</a></div>
-<div class="row modal" id="modal">
-  <table id="mobile-table">
-    <thead>
-      <tr>
-        <th>Training Module</th>
-        <th>Full name</th>
-        <th>Date of Certification</th>
-      </tr>
-    </thead>
-    <tbody id="mobile_res">
-      <tr>
-        <td colspan="4" id="mobile-default">
-          <center>
-            <h2 style="color: #607D8B">Result will show up here</h2>
-          </center>
-        </td>
+<div class="modal" id="modal">
+  <div class="row">
+    <div class="">
+      <table id="mobile-table">
+        <thead>
+          <tr>
+            <th>Training Module</th>
+            <th>Full name</th>
+            <th>Date of Certification</th>
+          </tr>
+        </thead>
+        <tbody id="mobile_res">
+          <tr>
+            <td colspan="4" id="mobile-default">
+              <center>
+                <h2 style="color: #607D8B">Result will show up here</h2>
+              </center>
+            </td>
 
-        <td colspan="4" id="mobile-no_data" style="display: none">
-          <center>
-            <h2 style="color: #d50000">No Data Found</h2>
-          </center>
-        </td>
+            <td colspan="4" id="mobile-no_data" style="display: none">
+              <center>
+                <h2 style="color: #d50000">No Data Found</h2>
+              </center>
+            </td>
 
-        <td colspan="4" id="mobile-empty_field" style="display: none">
-          <center>
-            <h2 style="color: #FF6D00">Please don't leave input field empty</h2>
-          </center>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+            <td colspan="4" id="mobile-empty_field" style="display: none">
+              <center>
+                <h2 style="color: #FF6D00">Please don't leave input field empty</h2>
+              </center>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+
 </div>
--- application/views/includes/navbar-mobile.php
+++ application/views/includes/navbar-mobile.php
@@ -1,12 +1,12 @@
 <div class="row nav-mobile-container" id="navbar-mobile">
     <div class="mobile-logo">
-        <a href="<?= base_url() ?>index.php/nmp/index" id="back-button" style="font-size: 0.8rem;"><i style="font-size: 1rem;" class="fas fa-arrow-left brand-mobile"></i></a>
+        <a href="<?= base_url() ?>index.php/nmp/index" id="back-button"><i class="fas fa-arrow-left brand-mobile"></i></a>
     </div>
     <div class="filler-mobile">
     </div>
 
     <div id="mobile" onclick="funcDropdown()">
-        <a href="#" ><i style="font-size: 1rem;" class="fas fa-ellipsis-v"></i></a>
+        <a href="#" id="elippsis"><i class="fas fa-ellipsis-v"></i></a>
     </div>
 </div>
 

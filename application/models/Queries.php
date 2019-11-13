0e2bf06cc4761340edf4b55f6e1c3bfc16e9eee8
Aljon Tocayon <actwhiteHat27@gmail.com>
2019-11-13 13:38:13 +0800

    left join

--- application/models/Queries.php
+++ application/models/Queries.php
@@ -8,9 +8,9 @@ class Queries extends CI_Model
     $param = ['%'.$this->input->post('fname').'%','%'.$this->input->post('lname').'%',date('Y-m-d',strtotime($this->input->post('bday')))];
 			$sql = "SELECT concat(tbl_trainee.fname,' ',LEFT(tbl_trainee.mname,1),'. ',tbl_trainee.lname,' ',tbl_trainee.suffix) as name,tbl_module.module as module, concat(DATE_FORMAT(tbl_schedule.start, '%Y %b %d'),' to ', DATE_FORMAT(tbl_schedule.end, '%Y %b %d')) as duration, tbl_training.certnumber as cert_num
 					from trainee as tbl_trainee
-					INNER JOIN training as tbl_training on tbl_trainee.trid = tbl_training.trid
-					INNER JOIN schedule as tbl_schedule on tbl_training.code = tbl_schedule.code
-					INNER JOIN module as tbl_module on tbl_schedule.modcode = tbl_module.modcode
+					LEFT JOIN training as tbl_training on tbl_trainee.trid = tbl_training.trid
+					LEFT JOIN schedule as tbl_schedule on tbl_training.code = tbl_schedule.code
+					LEFT JOIN module as tbl_module on tbl_schedule.modcode = tbl_module.modcode
 					where tbl_trainee.fname LIKE ? and tbl_trainee.lname LIKE ? and bdate = ? group by tbl_training.code order by tbl_training.code";
 			$query = $this->db->query($sql,$param);
 			// var_dump($this->db->last_query());die();
@@ -21,9 +21,9 @@ class Queries extends CI_Model
       $param = [$this->input->post('certnum')];
 			$sql = "SELECT concat(tbl_trainee.fname,' ',LEFT(tbl_trainee.mname,1),'. ',tbl_trainee.lname,' ',tbl_trainee.suffix) as name, tbl_module.module as module, concat(DATE_FORMAT(tbl_schedule.start, '%Y %b %d'),' to ', DATE_FORMAT(tbl_schedule.end, '%Y %b %d')) as duration, tbl_training.certnumber as cert_num
 					from trainee as tbl_trainee
-          INNER JOIN training as tbl_training on tbl_trainee.trid = tbl_training.trid
-					INNER JOIN schedule as tbl_schedule on tbl_training.code = tbl_schedule.code
-					INNER JOIN module as tbl_module on tbl_schedule.modcode = tbl_module.modcode
+          LEFT JOIN training as tbl_training on tbl_trainee.trid = tbl_training.trid
+					LEFT JOIN schedule as tbl_schedule on tbl_training.code = tbl_schedule.code
+					LEFT JOIN module as tbl_module on tbl_schedule.modcode = tbl_module.modcode
 					where tbl_training.certnumber = ?";
 			$query = $this->db->query($sql,$param);
 			//var_dump($this->db->last_query());die();
@@ -35,9 +35,9 @@ class Queries extends CI_Model
     $param = ['%'.$this->input->post('fname').'%','%'.$this->input->post('lname').'%',date('Y-m-d',strtotime($this->input->post('bday'))),$this->input->post('module')];
 			$sql = "SELECT concat(tbl_trainee.fname,' ',LEFT(tbl_trainee.mname,1),'. ',tbl_trainee.lname,' ',tbl_trainee.suffix) as name, tbl_module.module as module, concat(DATE_FORMAT(tbl_schedule.start, '%Y %b %d'),' to ', DATE_FORMAT(tbl_schedule.end, '%Y %b %d')) as duration, tbl_training.certnumber as cert_num
 					from trainee as tbl_trainee
-					INNER JOIN training as tbl_training on tbl_trainee.trid = tbl_training.trid
-					INNER JOIN schedule as tbl_schedule on tbl_training.code = tbl_schedule.code
-					INNER JOIN module as tbl_module on tbl_schedule.modcode = tbl_module.modcode
+					LEFT JOIN training as tbl_training on tbl_trainee.trid = tbl_training.trid
+					LEFT JOIN schedule as tbl_schedule on tbl_training.code = tbl_schedule.code
+					LEFT JOIN module as tbl_module on tbl_schedule.modcode = tbl_module.modcode
 					where tbl_trainee.fname LIKE ? and tbl_trainee.lname LIKE ? and bdate = ? and tbl_module.modcode = ? group by tbl_training.code order by tbl_training.code";
 			$query = $this->db->query($sql,$param);
 			//var_dump($this->db->last_query());die();

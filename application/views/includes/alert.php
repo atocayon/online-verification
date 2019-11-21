6896571bf45f47c425b865ce0f0bac38105e0a95
Aljon Tocayon <actwhiteHat27@gmail.com>
2019-11-21 17:01:29 +0800

    temp-mobile alert modal

--- application/views/includes/alert.php
+++ application/views/includes/alert.php
@@ -2,14 +2,14 @@
   <div class="privacy-alert">
     <div class="row center">
       <div class="">
-        <h1 style="color: red;"><i class="fas fa-exclamation-triangle"></i> ATTENTION <i class="fas fa-exclamation-triangle"></i></h1>
+        <h1><i class="fas fa-exclamation-triangle"></i> ATTENTION <i class="fas fa-exclamation-triangle"></i></h1>
       </div>
     </div>
 
     <div class="row center">
       <div class="">
-        <h3 style="text-align: center;">
-          This system is monitored and is intended for use on verification of Name, Certificate Number and PDC Enrollees of NMP Trainees.
+        <h3>
+          This system is monitored and is intended to use for verification of Name, Certificate Number and PDC Enrollees of NMP Trainees.
           Any misuse or manipulation of the data provided in the search result will be subjected to RA No. 10173 otherwise known
           as the Data Privacy Act. If you have some question or clarification kindly check the FAQ page or <a href="#">Click Here</a> to contact the National Maritime Polytechnic.
         </h3>
--- application/views/includes/loading.php
+++ application/views/includes/loading.php
@@ -1,1 +1,1 @@
-<h1>Loading...Please Wait...</h1>
+<h1>Loading...Please Wait</h1>
